4aa8f8546f41294d468cee728a3acefbfcf3a5af
jesus yepes <jesusyepes.1205@gmail.com>
2021-06-15 03:22:46 -0400

    Definiendo rutas del servicio

--- src/app/common/const/path.ts
+++ src/app/common/const/path.ts
@@ -1,12 +1,13 @@
-// ruta para los servicios web
+// ruta para los servicios web heroku
 export const AppUrlBase = 
 {
     URL: 'http://hp-api.herokuapp.com/api/characters'
 };
 
-
+// Endpoint para cada opcion
 export const ServicePath =
 {
-    AUTH_API: `${AppUrlBase.URL}/`,
-    USER: `${AppUrlBase.URL}/`
+    HOUSE: `${AppUrlBase.URL}/house/`, // nombre de la casa: /house/{{nombreCasa}}
+    STUDENT: `${AppUrlBase.URL}/students`, // Lista de estudiantes
+    STAFF: `${AppUrlBase.URL}/staff` // Lista de profesores
 }
