f8fb611d995c8f247d19c35a22f7846a6635c852
jesus yepes <jesusyepes.1205@gmail.com>
2021-06-15 03:44:44 -0400

    Agregando la opcion de consulta de profesores en la opcion de profesores

--- src/app/pages/profesores/profesores.component.ts
+++ src/app/pages/profesores/profesores.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewChild} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ClientService } from 'src/app/core/services/client.service';
+import * as moment from 'moment';
+
+//tabla profesores
+export class ProfesoresElement {
+  Codigo!: number;
+  Name!: string;
+  Patronus!: string;  
+  Age!: number;
+  Image!: string;
+}
+
 
 @Component({
   selector: 'app-profesores',
   templateUrl: './profesores.component.html',
   styleUrls: ['./profesores.component.css']
 })
+
 export class ProfesoresComponent implements OnInit {
 
-  constructor() { }
+  // tabla personajes
+  displayedColumns: string[] = ['Codigo', 'Name', 'Patronus', 'Age', 'Image'];
+  dataSource!: MatTableDataSource<ProfesoresElement>;
+  @ViewChild('paginatorProfesores') paginatorPersonas!: MatPaginator;
+  // fin tabla personajes
+  ProfesoresElement = [];
+
+  listProfesoresElement : ProfesoresElement [] = [];
+
+
+  selectedHouse!: string;
+  
+  constructor(private clientService: ClientService) { }
 
   ngOnInit(): void {
+  }
+
+
+  // boton para consultar
+  consultar(event: Event) : any{
+    console.log('data', event);
+    
+    this.clientService.getStaft().subscribe(
+      data => {
+        let i=1;//para simular el codigo como indice
+        data.forEach(((object: { name: string; patronus: string; image: string; dateOfBirth: Date; }) => {              
+            let dataTable = new ProfesoresElement();
+            dataTable.Codigo = i;
+            dataTable.Name = object.name;
+            dataTable.Patronus = object.patronus;
+            dataTable.Image = object.image;
+           // calculo de edad
+            var birthyear = moment(new Date().getFullYear(), 'YYYY'); 
+            var visitdate = moment(object.dateOfBirth, 'DD-MM-YYYY');
+            if (!Number.isNaN(birthyear.diff(visitdate, 'y'))) 
+                dataTable.Age = birthyear.diff(visitdate, 'y');
+            else  
+              dataTable.Age = 0; 
+             
+            i++;
+            this.listProfesoresElement.push(dataTable);
+        }));
+
+        console.log(this.listProfesoresElement);
+
+        this.dataSource = new MatTableDataSource<ProfesoresElement>(this.listProfesoresElement);
+        this.dataSource.paginator = this.paginatorPersonas;
+      },
+      err => {
+      
+        console.log(err);
+        window.alert('Ha ocurrido un error. Favor intente mas tarde.');
+      }
+    );
   }
 
 }
